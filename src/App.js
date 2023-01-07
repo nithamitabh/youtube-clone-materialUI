2a815773124a26315f49f52d5715f366c77002e0
nithamitabh <96220322+nithamitabh@users.noreply.github.com>
2023-01-07 19:42:50 +0530

    static navbar ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Box } from '@mui/system';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Navbar from './components/Navbar';
+import Feed from './components/Feed';
+import VideoDetail from './components/VideoDetail';
+import Channel from './components/Channel';
+import SearchFeed from './components/SearchFeed';
 
-export default App;
+
+const App = () => (
+  <BrowserRouter>
+  <Box sx={{backgroundColor: '#000'}}>
+    <Navbar/>
+    <Routes>
+        <Route exact path='/'element = {<Feed/>}/>
+        <Route path='/video/:id' element = {<VideoDetail/>}/>
+        <Route path='/channel/:id' element ={<Channel/>}/>
+        <Route path='/search/:searchTerm' element ={<SearchFeed/>}/>
+    </Routes>
+  </Box>
+  </BrowserRouter>
+);
+
+export default App
